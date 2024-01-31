d53135a1354337aa455c29a5446d96c2fff77a75
gokhangultan <gultangokhan@gmail.com>
2024-02-01 01:47:10 +0300

    responsive tailwind changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,22 +4,22 @@ import { faMoon, faToggleOff } from '@fortawesome/free-solid-svg-icons'
 export default function Header() {
     return (
         <div className="pt-[23px]">
-            <div className="flex gap-[14px] float-end">
+            <div className="flex gap-[14px] float-right">
                  <button><span className='bg-yellow-600 rounded-md p-1'><FontAwesomeIcon icon={faMoon} /> <FontAwesomeIcon icon={faToggleOff} /></span> LIGHT MODE</button>
                  <p>|</p>
                   <span>TÜRKÇE'YE GEÇ</span>
             </div>
-            <div className="pt-5">            
+            <div className='flex justify-between mt-16'>
+            <div className="  flex-1">            
                 <img src="logo.gg2.png" className=" w-[83.68px] h-[82px] " />
             </div>
-            <div className=" pt-0 ">
-            <div className="float-end"><nav className="flex justify-between gap-10 ">
+            <div className=" flex justify-between">
+                <nav className='flex gap-10'>
                 <button className="text-[#6B7280]">Skills</button>
                 <button className="text-[#6B7280]">Projects</button>
                 <button className="text-[#3730A3] bg-white border-[#3730A3] py-[12px] px-[32px] rounded">Hire me</button>
-            </nav>
-            </div>
-            <div>
+                </nav>
+            <div></div>
             </div>
          </div>
             
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,22 +2,23 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 export default function Hero() {
     return (
-        <div className="pt-[150px] hero-image flex justify-between">
+        <div className="pt-10 hero-image flex justify-between gap-5">
             <div className="left-hero flex-1 ">
                  <div className=" flex-col ">
                    <h3 className="text-[#BAB2E7] text-xl">Gökhan GÜLTAN</h3>
             <hr/>
                      <h1 className="text-[72px] text-[#AEBCCF] font-medium mt-10">Creative thinker <br/>Minimalism Lover</h1>
-                     <p className="text-[18px] text-white mt-10">Hi, I'm Gokhan. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
-            <div className="flex gap-4 mt-10">
+                     <p className="text-[18px] text-white mt-10">Hi, I'm Gokhan. Lorem Ipsum is simply dummy text of the printing and typesetting industry. <br/>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+            <div className="flex gap-5 mt-10">
                  <button className="text-[#3730A3] bg-white border-[#3730A3] py-[12px] px-[32px] rounded">Hire me</button>
                  <button className="text-[#E1E1FF] bg-[#383838] border-[#3730A3] py-[12px] px-[32px] rounded"><FontAwesomeIcon icon={faGithub} /> Github</button>
                  <button className="text-[#E1E1FF] bg-[#383838] border-[#3730A3] py-[12px] px-[32px] rounded"><FontAwesomeIcon icon={faLinkedin} /> Linkedin</button>
             </div>
         </div>
         </div>
-        <div className='flex'>
-        <div className='right-hero float-end'><img src="hero2.png"  className=" rounded-2xl w-[476px] h-[375px]"/></div></div>
+        <div className='flex right-hero'>
+        <div className=' mt-10'><img src="hero2.png"  className=" rounded-2xl w-[476px] h-[375px] ml-16 "/></div>
+        </div>
 
         
         </div>
