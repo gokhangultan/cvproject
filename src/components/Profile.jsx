57c567c7fe71f93e889069450b3f15986b1b6c9a
gokhangultan <gultangokhan@gmail.com>
2024-01-31 09:48:24 +0300

    some css changes skills profiles sections

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,5 +1,38 @@
 export default function Profile() {
     return (
-        <div><h1>Profile page</h1></div>
+        <div className="pb-10">
+         <h1 className="pt-10 pb-3 text-5xl">Profile</h1>
+            <div class="flex flex-row ">
+  <div class="basis-1/3 pb-10">
+  <h2 className="text-3xl text-[#B7AAFF] pt-2 pb-5">Profile</h2>
+  <table>
+  <tr>
+  <td className="pr-5 font-bold">Doğum Tarihi</td>
+    <td className="pr-5">Maria Anders</td>
+  </tr>
+  <tr>
+    <td className="pr-5 font-bold">İkamet Şehri</td>
+    <td className="pr-5">Francisco Chang</td>
+  </tr>
+  <tr>
+  <td className="pr-5 font-bold">Eğitim Durumu</td>
+    <td className="pr-5">Celal Bayar Ünv. Bilgisayar Müh. Lisans, 2018</td>
+  </tr>
+ 
+  <tr>
+    <td className="pr-5 font-bold">Tercih Ettiği Rol</td>
+    <td className="pr-5">Team Lead</td>
+  </tr>
+</table>
+  </div>
+  <div class="basis-2/3 px-10">
+  <h2 className="text-3xl text-[#B7AAFF] pt-2">About Me</h2>
+  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam aut, odit laborum aliquam voluptatum nisi mollitia.</p>
+  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam aut, odit laborum aliquam voluptatum nisi mollitia.</p>
+    </div>
+    
+</div>
+<hr/>    
+        </div>
     )
 }
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,37 +2,38 @@ export default function Skills() {
     return (
         <div className="pt-10">
             <hr/>
-            <h1 className="pt-10 pb-3">Skills</h1>
-        <div className="flex justify-between">
+            <h1 className="pt-10 pb-3 text-5xl">Skills</h1>
+        <div className="flex justify-between gap-20">
         
         <div>
-            <h2>Java Script</h2>
-            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
+            <h2 className="text-3xl text-[#B7AAFF] pt-2">Java Script</h2>
+            <p className="text-xs pt-5">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
         </div>
         <div>
-            <h2>React Js</h2>
-            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
+            <h2 className="text-3xl text-[#B7AAFF] pt-2">React Js</h2>
+            <p className="text-xs pt-5">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
         </div>
         <div>
-            <h2>Node Js</h2>
-            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
+            <h2 className="text-3xl text-[#B7AAFF] pt-2">Node Js</h2>
+            <p className="text-xs pt-5">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
         </div>
 
         </div>
-        <div className="flex justify-between pt-5">
+        <div className="flex justify-between pt-5 gap-20">
         <div >
-            <h2>Server Administrator</h2>
-            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
+            <h2 className="text-3xl text-[#B7AAFF] pt-2">Server Administrator</h2>
+            <p className="text-xs pt-5">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
         </div>
         <div>
-            <h2>Blockchain Developer</h2>
-            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
+            <h2 className="text-3xl text-[#B7AAFF] pt-2">Blockchain Developer</h2>
+            <p className="text-xs pt-5">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
         </div>
         <div>
-            <h2>Team Lead</h2>
-            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
+            <h2 className="text-3xl text-[#B7AAFF] pt-2">Team Lead</h2>
+            <p className="text-xs pt-5 pb-10">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
         </div>
+        </div><hr/>
         </div>
-        </div>
+        
     )
 }
