7dd86a6f65b0ebe1ad782eacebcc7696364a706c
gokhangultan <gultangokhan@gmail.com>
2024-01-31 17:33:13 +0300

    footer a bit changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,21 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {  faHandPointRight } from '@fortawesome/free-solid-svg-icons'
 export default function Footer() {
     return (
-        <div><h1>Footer page</h1></div>
+        <div className="bg-[#141414] mb-10">
+            <div>
+                <h1 className="mb-20">Let’s work together on <br/>your next product.</h1>
+            <div className="flex justify-between">
+                <div className='flex-1'>
+                     <p className='underline'><FontAwesomeIcon icon={faHandPointRight} /> gultangokhan@gmail.com</p>
+                     </div>
+                <div className='flex gap-3'>
+                    <button>Personal Blog</button>
+                <button>Github</button>
+                <button>Linkedin</button>
+                </div>
+                </div>
+            </div>
+        </div>
     )
 }
