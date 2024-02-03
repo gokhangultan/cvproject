8c8a7705cafa5030ebd868928a7dfa5e9586e9d2
gokhangultan <gultangokhan@gmail.com>
2024-02-04 02:06:13 +0300

    useAxios hook footer project1 testing MOCKAPI

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,32 @@
+import React, { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {  faHandPointRight } from '@fortawesome/free-solid-svg-icons'
+import useAxios from '../hooks/useAxios';
+
+
 export default function Footer() {
+   
+    const apiUrl = 'https://65be832adcfcce42a6f29918.mockapi.io/api/v1/cvData';
+  const { data: cvDataArray, loading, error } = useAxios(apiUrl);
+
+  // Check if loading
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  // Check if there's an error
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
+
+
+  // BECAUSE DUMMY MOCKAPI REQUIRE DATA FORMAT AS JSON MUST BE AN ARRAY.... SPENT 4hours cuz of that
+  const cvData = cvDataArray[0];
+
     return (
         <div className="bg-[#F9F9F9] dark:bg-[#141414] py-10 pl-[125px] pr-[175px]">
             <div>
-                <h1 className="mb-20 dark:text-#[AEBCCF] text-[#1F2937] font-semibold text-[42px]">Let’s work together on <br/>your next product.</h1>
+                <h1 className="mb-20 dark:text-#[AEBCCF] text-[#1F2937] font-semibold text-[42px]"> {cvData.en.projects[0].projectTitle}<br/>your next product.</h1>
             <div className="flex justify-between">
                 <div className='flex-1'>
                      <p className='font-medium underline text-[#AF0C48] dark:text-[#BAB2E7]'><FontAwesomeIcon icon={faHandPointRight} /> gultangokhan@gmail.com</p>
