7ecdd7d7be11b3caf1a4266a714fa189f1abcf35
gokhangultan <gultangokhan@gmail.com>
2024-02-06 03:19:13 +0300

    css bug

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,7 +27,7 @@ export default function Hero() {
                      <h1 className="xl:text-[72px] lg:text-[50px]  sm:text-[40px] text-[40px] dark:text-[#AEBCCF] text-[#1F2937] font-bold mt-3 xl:mt-10 lg:mt-5 ">{heroData.heroTitle}</h1>
                      <p className="text-[18px] text-[#6B7280] dark:text-white mt-3 xl:mt-10 lg:mt-8 md:mt-5 ">{heroData.heroDescription}</p>
             <div className="flex justify-start gap-2 xl:mt-10 mt-3 lg:mt-8 md:mt-5">
-                 <button className="dark:text-[#3730A3] text-white bg-[#3730A3] dark:bg-[#E1E1FF] border-[#3730A3] border-2 xl:py-[12px] xl:px-[32px] lg:py-[8px] lg:px-[24px] py-[6px] px-[18px] rounded font-medium">Hire me</button>
+            <a href={`${heroData.gitLink}`}><button className="dark:text-[#3730A3] text-white bg-[#3730A3] dark:bg-[#E1E1FF] border-[#3730A3] border-2 xl:py-[12px] xl:px-[32px] lg:py-[8px] lg:px-[24px] py-[6px] px-[18px] rounded font-medium">Hire me</button></a>
                  <a href={`${heroData.gitLink}`} target='_blank'><button className="dark:text-[#E1E1FF] text-[#3730A3] dark:bg-[#383838] bg-white border-[#3730A3]  border-2 xl:px-[32px] lg:py-[8px] lg:px-[24px] py-[6px] px-[18px] rounded font-medium"><FontAwesomeIcon icon={faGithub} /> Github</button></a>
                  <a href={`${heroData.webLink}`} target='_blank'><button className="dark:text-[#E1E1FF] text-[#3730A3] dark:bg-[#383838] bg-white border-[#3730A3] border-2 xl:px-[32px] lg:py-[8px] lg:px-[24px] py-[6px] px-[18px] rounded font-medium"><FontAwesomeIcon icon={faLinkedin} /> Linkedin</button></a>
             </div>
