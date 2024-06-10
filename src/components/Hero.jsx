5bb56893e7eb94d2d405ec7100f360db16c061b8
gokhangultan <gultangokhan@gmail.com>
2024-06-10 14:11:20 +0300

    css and content revisions

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,35 +1,68 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
-import { useSelector } from 'react-redux';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import {
+  faClock,
+  faFile,
+  faFileAlt,
+} from "@fortawesome/free-regular-svg-icons";
+import { useSelector } from "react-redux";
 export default function Hero() {
+  const lang = useSelector((store) => store.lang);
+  const data = useSelector((store) => store.data);
 
-    const lang = useSelector((store) => store.lang);
-    const data = useSelector((store) => store.data);
+  const heroData = lang === "EN" ? data.en.hero : data.tr.hero;
+  const constData = lang === "EN" ? data.en.const : data.tr.const;
 
-    const heroData = lang === 'EN' ? data.en.hero : data.tr.hero;
-    const constData = lang === 'EN' ? data.en.const : data.tr.const;
-
-    return (
-        <div className=" flex flex-col lg:flex-row px-[20px] md:px-[190px] gap-3">
-            <div className="left-hero basis-3/5 ">
-                <div className=" flex-col ">
-                    <h3 className="dark:text-[#BAB2E7] text-[#4338CA] text-xl font-medium">{heroData.heroName}</h3>
-                    <hr />
-                    <h1 className="xl:text-[72px] lg:text-[50px] md:text-[40px] sm:text-[32px] text-[28px] dark:text-[#AEBCCF] text-[#1F2937] font-bold mt-3 xl:mt-10 lg:mt-5 ">{heroData.heroTitle}</h1>
-                    <h1 className="xl:text-[72px] lg:text-[50px] md:text-[40px] sm:text-[32px] text-[28px] dark:text-[#AEBCCF] text-[#1F2937] font-bold  ">{heroData.heroTitle2}</h1>
-                    <p className="text-[18px] text-[#6B7280] dark:text-white mt-3 xl:mt-10 lg:mt-8 md:mt-5 ">{heroData.heroDescription}</p>
-                    <div className="flex justify-center gap-2 xl:mt-10 mt-3 lg:mt-8 md:mt-5">
-                        <a href={`${heroData.gitLink}`}><button className="dark:text-[#3730A3] text-white bg-[#3730A3] dark:bg-[#E1E1FF] border-[#3730A3] border-2 xl:py-[8px] xl:px-[40px] lg:py-[8px] lg:px-[32px] md:py-[6px] md:px-[30px] py-[6px] px-[20px] rounded font-medium">{constData.constHireMe}</button></a>
-                        <a href={`${heroData.gitLink}`} target='_blank'><button className="dark:text-[#E1E1FF] text-[#3730A3] dark:bg-[#383838] bg-white border-[#3730A3]  border-2 xl:px-[32px] lg:py-[8px] lg:px-[24px] py-[6px] px-[18px] rounded font-medium"><FontAwesomeIcon icon={faGithub} /> {constData.constGithub}</button></a>
-                        <a href={`${heroData.webLink}`} target='_blank'><button className="dark:text-[#E1E1FF] text-[#3730A3] dark:bg-[#383838] bg-white border-[#3730A3] border-2 xl:px-[32px] lg:py-[8px] lg:px-[24px] py-[6px] px-[18px] rounded font-medium"><FontAwesomeIcon icon={faLinkedin} /> {constData.constLinkedin}</button></a>
-                    </div>
-                </div>
-            </div>
-            <div className='right-hero  basis-2/5 items-center align-middle justify-center flex'>
-                <div className=' mt-3 '><img src="hero2.png" className=" rounded-2xl w-[476px] h-[375px] " alt='heroprofilfoto' /></div>
-            </div>
-
-
+  return (
+    <div className=" flex flex-col lg:flex-row px-[20px] md:px-[190px] gap-3">
+      <div className="left-hero basis-3/5 ">
+        <div className=" flex-col ">
+          <h3 className="dark:text-[#BAB2E7] text-[#4338CA] text-xl font-medium">
+            {heroData.heroName}
+          </h3>
+          <hr />
+          <h1 className="xl:text-[72px] lg:text-[50px] md:text-[40px] sm:text-[32px] text-[28px] dark:text-[#AEBCCF] text-[#1F2937] font-bold mt-3 xl:mt-10 lg:mt-5 ">
+            {heroData.heroTitle}
+          </h1>
+          <h1 className="xl:text-[72px] lg:text-[50px] md:text-[40px] sm:text-[32px] text-[28px] dark:text-[#AEBCCF] text-[#1F2937] font-bold  ">
+            {heroData.heroTitle2}
+          </h1>
+          <p className="text-[18px] text-[#6B7280] dark:text-white mt-3 xl:mt-10 lg:mt-8 md:mt-5 ">
+            {heroData.heroDescription}
+          </p>
+          <div className="flex justify-center gap-2 xl:mt-10 mt-3 lg:mt-8 md:mt-5">
+            <a href={`${heroData.gitLink}`}>
+              <button className="dark:text-[#3730A3] text-white bg-[#3730A3] dark:bg-[#E1E1FF] border-[#3730A3] border-2 xl:py-[8px] xl:px-[40px] lg:py-[8px] lg:px-[32px] md:py-[6px] md:px-[30px] py-[6px] px-[20px] rounded font-medium">
+                {constData.constHireMe}
+              </button>
+            </a>
+            <a href={`${heroData.gitLink}`} target="_blank">
+              <button className="dark:text-[#E1E1FF] text-[#3730A3] dark:bg-[#383838] bg-white border-[#3730A3]  border-2 xl:px-[32px] lg:py-[8px] lg:px-[24px] py-[6px] px-[18px] rounded font-medium">
+                <FontAwesomeIcon icon={faGithub} /> {constData.constGithub}
+              </button>
+            </a>
+            <a href={`${heroData.webLink}`} target="_blank">
+              <button className="dark:text-[#E1E1FF] text-[#3730A3] dark:bg-[#383838] bg-white border-[#3730A3] border-2 xl:px-[32px] lg:py-[8px] lg:px-[24px] py-[6px] px-[18px] rounded font-medium">
+                <FontAwesomeIcon icon={faLinkedin} /> {constData.constLinkedin}
+              </button>
+            </a>
+            <a href="/gokhangultan_cv.pdf" target="_blank">
+              <button className="dark:text-[#E1E1FF] text-[#3730A3] dark:bg-[#383838] bg-white border-[#3730A3] border-2 xl:px-[32px] lg:py-[8px] lg:px-[24px] py-[6px] px-[18px] rounded font-medium">
+                <FontAwesomeIcon icon={faFileAlt} /> CV
+              </button>
+            </a>
+          </div>
         </div>
-    )
+      </div>
+      <div className="right-hero  basis-2/5 items-center align-middle justify-center flex">
+        <div className=" mt-3 ">
+          <img
+            src="hero2.png"
+            className=" rounded-2xl w-[476px] h-[375px] "
+            alt="heroprofilfoto"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,11 +1,8 @@
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import useAxios from "../hooks/useAxios";
 
-
 export default function projectCard({ data }) {
-
-
-  const apiUrl = 'https://65be832adcfcce42a6f29918.mockapi.io/api/v1/cvData';
+  const apiUrl = "https://65be832adcfcce42a6f29918.mockapi.io/api/v1/cvData";
   const { data: cvDataArray, loading, error } = useAxios(apiUrl);
   if (loading) {
     return <p>Loading...</p>;
@@ -21,22 +18,38 @@ export default function projectCard({ data }) {
 
   return (
     <div className=" mb-5 w-[400px] h-auto">
-      <img src={data.projectImg} className="rounded  w-[250px] h-[250px] object-contain p-2 align-middle justify-center items-center" alt={data.projectTitle} />
-      <h3 className="dark:text-[#CFCBFF] text-[#4338CA] xl:text-3xl lg:text-2xl text-xl font-medium">{data.projectTitle}</h3>
-      <p className="xl:text-sm text-xs my-5 dark:text-white text-[#6B7280]">{data.projectDescription}</p>
+      <img
+        src={data.projectImg}
+        className="rounded  w-[250px] h-[250px] object-contain p-2 align-middle justify-center items-center"
+        alt={data.projectTitle}
+      />
+      <h3 className="dark:text-[#CFCBFF] text-[#4338CA] xl:text-3xl lg:text-2xl text-xl font-medium">
+        {data.projectTitle}
+      </h3>
+      <p className="xl:text-sm text-xs my-5 dark:text-white text-[#6B7280]">
+        {data.projectDescription}
+      </p>
       <div className="xl:pb-5 lg:pb-3 md:pb-2 pb-1 flex flex-wrap flex-row gap-2">
         {data.tags.map((tag, index) => (
-          <button key={index} className="xl:font-medium font-bold flex-1  dark:text-[#E1E1FF] text-[#3730A3] xl:text-base text-xs border-2 border-[#3730A3]  bg-gray-100 rounded px-4 py-1">
+          <button
+            key={index}
+            className="xl:font-medium font-bold flex-1  dark:text-[#5d5df5] text-[#3730A3] xl:text-base text-xs border-2 border-[#3730A3]  bg-gray-100 rounded px-4 py-1"
+          >
             {tag}
           </button>
         ))}
       </div>
       <div className="flex justify-between">
         <a href={data.gitLink} target="_blank" rel="noopener noreferrer">
-          <button className="xl:font-medium font-bold flex-1  dark:text-[#E1E1FF] text-[#3730A3] xl:text-base text-xs border-2 border-[#3730A3]  bg-gray-300 rounded px-4 py-1"> Github</button>
+          <button className="xl:font-medium font-bold flex-1  dark:text-[#5d5df5] text-[#3730A3] xl:text-base text-xs border-2 border-[#3730A3]  bg-gray-300 rounded px-4 py-1">
+            {" "}
+            Github
+          </button>
         </a>
         <a href={data.webLink} target="_blank" rel="noopener noreferrer">
-          <button className="xl:font-medium font-bold flex-1  dark:text-[#E1E1FF] text-[#3730A3] xl:text-base text-xs border-2 border-[#3730A3]  bg-gray-300 rounded px-4 py-1	">View Site</button>
+          <button className="xl:font-medium font-bold flex-1  dark:text-[#5d5df5] text-[#3730A3] xl:text-base text-xs border-2 border-[#3730A3]  bg-gray-300 rounded px-4 py-1	">
+            View Site
+          </button>
         </a>
       </div>
     </div>
